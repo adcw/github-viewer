db9e0c6ce264e6da6dafddefbee56523d992de34
Adrian Ćwiąkała <61947288+adcw@users.noreply.github.com>
2024-05-20 14:56:04 +0200

    Display current user name

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -1,22 +1,40 @@
-import { AppShell, Button, Center, Group, TextInput } from "@mantine/core";
+import {
+  AppShell,
+  Button,
+  Center,
+  Group,
+  TextInput,
+  Text,
+  Container,
+} from "@mantine/core";
 
 import { useGithubAccessToken } from "../github-utils/GHAccessTokenProvider";
 import { Octokit } from "octokit";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Page = () => {
   const accessToken = useGithubAccessToken();
 
-  const getUser = useCallback(async () => {
-    try {
-      const octokit = new Octokit({ auth: accessToken });
-      const data = await octokit.rest.users.getAuthenticated();
+  const [userData, setUserData] = useState(undefined);
 
-      return data;
-    } catch (error) {
-      return {};
-    }
-  }, [accessToken]);
+  const getUser = useCallback(
+    async (at: string) => {
+      try {
+        const octokit = new Octokit({ auth: at });
+        const res = await octokit.rest.users.getAuthenticated();
+
+        setUserData(res.data);
+      } catch (error) {
+        console.log("ERR\n" + error);
+        return {};
+      }
+    },
+    [accessToken]
+  );
+
+  useEffect(() => {
+    getUser(accessToken);
+  }, [accessToken, getUser]);
 
   return (
     <AppShell
@@ -28,17 +46,19 @@ const Page = () => {
       padding="md"
     >
       <AppShell.Header>
-        <form>
-          <Center>
-            <Group gap={0}>
-              <TextInput w={300} placeholder="Enter github username" />
-              <Button>Search</Button>
-            </Group>
-          </Center>
-        </form>
+        <Group pt="sm" gap="md" grow>
+          <Group gap={4}>
+            <Text>Authenticated as</Text>
+            <Text color="yellow">{userData?.login}</Text>
+          </Group>
+          <Group gap={0}>
+            <TextInput w={300} placeholder="Enter github username" />
+            <Button>Search</Button>
+          </Group>
+        </Group>
       </AppShell.Header>
 
-      <AppShell.Main>{JSON.stringify(getUser(), null, 4)}</AppShell.Main>
+      <AppShell.Main></AppShell.Main>
     </AppShell>
   );
 };
